ff51b80613db02c7ad4eedf27d8cf0a0d7f4bce7
always0ne <si8363@naver.com>
2020-09-18 00:49:14 +0900

    refactor: H.txt has a value

--- src/03/generate_hash.py
+++ src/03/generate_hash.py
@@ -6,9 +6,10 @@ def generate_hash(filename):
 	password = input("Key : ")
 	h = HMAC.new(password.encode('utf-8'))
 	with open(filename, 'rb') as message:
-		h.update(message.read())
+		msg_string = message.read()[:-1]
+		h.update(msg_string)
 		print(h.digest())
 		with open("H.txt",'wb') as hash_value:
-			hash_value.write(h.digest())
+			hash_value.write(msg_string + h.digest())
 	
 generate_hash('1.txt')
--- src/03/validate_hash.py
+++ src/03/validate_hash.py
@@ -5,13 +5,12 @@ from Crypto.Hash import HMAC
 def check_hash(filename):
 	password = input("Key : ")
 	h = HMAC.new(password.encode('utf-8'))
-	with open(filename, 'rb') as message:
-		h.update(message.read())
+	with open('H.txt', 'rb') as message:
+		h.update(message.read(os.path.getsize("H.txt")-16))
 		print(h.digest())
-		with open("H.txt",'rb') as hash_value:
-			if hash_value.read() == h.digest():
-				print("OK")
-			else:
-				print("NOK")
+		if h.digest() == message.read():
+			print("OK")
+		else:
+			print("NOK")
 	
 check_hash('1.txt')
